c26cf1ee673d6a969d2442da6ec7704d5abf6997
Colin Bruner <code@colinbruner.com>
2025-01-03 09:08:26 -0600

    switch aws api op call to be function based, rather than automatically on new shell

--- files/zsh/zsh.d/31-aws.zsh
+++ files/zsh/zsh.d/31-aws.zsh
@@ -2,8 +2,11 @@
 # NOTE: Waiting on this to be out of public beta
 # https://developer.1password.com/docs/cli/shell-plugins/terraform/
 # NOTE: Disabled in VSCode Shell
-if [[ $(uname) == "Darwin" ]] && [[ -n $(which op) ]] && [[ -z $(env | grep VSCODE) ]]; then
-  export AWS_ACCESS_KEY_ID=$(op read "op://homelab/AWS Access Key/access key id")
-  export AWS_SECRET_ACCESS_KEY=$(op read "op://homelab/AWS Access Key/secret access key")
-  export AWS_REGION="us-east-2"
-fi
+# NOTE: Super annoying given how frequently I open up shells, wrapping in a function
+function awsAuth() {
+  if [[ $(uname) == "Darwin" ]] && [[ -n $(which op) ]] && [[ -z $(env | grep VSCODE) ]]; then
+    export AWS_ACCESS_KEY_ID=$(op read "op://homelab/AWS Access Key/access key id")
+    export AWS_SECRET_ACCESS_KEY=$(op read "op://homelab/AWS Access Key/secret access key")
+    export AWS_REGION="us-east-2"
+  fi
+}
